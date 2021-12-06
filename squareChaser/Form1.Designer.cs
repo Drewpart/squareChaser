b6505b2ab1bd846abf241b69271eaace126c9059
P4 <andrpart669@ed.amdsb.ca>
2021-12-06 13:50:27 -0500

    Add project files.

---
+++ squareChaser/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace squareChaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p2Score = new System.Windows.Forms.Label();
+            this.p1Score = new System.Windows.Forms.Label();
+            this.endLabel = new System.Windows.Forms.Label();
+            this.yesButton = new System.Windows.Forms.Button();
+            this.noButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // p2Score
+            // 
+            this.p2Score.AutoSize = true;
+            this.p2Score.ForeColor = System.Drawing.Color.White;
+            this.p2Score.Location = new System.Drawing.Point(382, 22);
+            this.p2Score.Name = "p2Score";
+            this.p2Score.Size = new System.Drawing.Size(13, 13);
+            this.p2Score.TabIndex = 0;
+            this.p2Score.Text = "0";
+            // 
+            // p1Score
+            // 
+            this.p1Score.AutoSize = true;
+            this.p1Score.ForeColor = System.Drawing.Color.White;
+            this.p1Score.Location = new System.Drawing.Point(170, 22);
+            this.p1Score.Name = "p1Score";
+            this.p1Score.Size = new System.Drawing.Size(13, 13);
+            this.p1Score.TabIndex = 1;
+            this.p1Score.Text = "0";
+            // 
+            // endLabel
+            // 
+            this.endLabel.AutoSize = true;
+            this.endLabel.BackColor = System.Drawing.Color.Transparent;
+            this.endLabel.Location = new System.Drawing.Point(119, 166);
+            this.endLabel.Name = "endLabel";
+            this.endLabel.Size = new System.Drawing.Size(0, 13);
+            this.endLabel.TabIndex = 2;
+            // 
+            // yesButton
+            // 
+            this.yesButton.Location = new System.Drawing.Point(287, 92);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(75, 23);
+            this.yesButton.TabIndex = 3;
+            this.yesButton.Text = "Yes";
+            this.yesButton.UseVisualStyleBackColor = true;
+            this.yesButton.Visible = false;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // noButton
+            // 
+            this.noButton.Location = new System.Drawing.Point(287, 142);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(75, 23);
+            this.noButton.TabIndex = 4;
+            this.noButton.Text = "No";
+            this.noButton.UseVisualStyleBackColor = true;
+            this.noButton.Visible = false;
+            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.yesButton);
+            this.Controls.Add(this.endLabel);
+            this.Controls.Add(this.p1Score);
+            this.Controls.Add(this.p2Score);
+            this.Name = "Form1";
+            this.Text = "Square Chaser";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p2Score;
+        private System.Windows.Forms.Label p1Score;
+        private System.Windows.Forms.Label endLabel;
+        private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Button noButton;
+    }
+}
+
---
+++ squareChaser/Form1.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace squareChaser
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(165, 120, 40, 40);
+        Rectangle player2 = new Rectangle(360, 120, 40, 40);
+        Rectangle ball = new Rectangle(295, 195, 10, 10);
+        Rectangle speed = new Rectangle(170, 200, 10, 10);
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        int playerSpeed2 = 2;
+        int playerSpeed1 = 2;
+        int ballSpot;
+        int speedSpot;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool leftArrow = false;
+        bool rightArrow = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush SolidBrush = new SolidBrush(Color.Orange);
+        SolidBrush YellowBrush = new SolidBrush(Color.Yellow);
+
+        Random ballLoca = new Random();
+        Random speedMvmt = new Random();
+
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Left:
+                    leftArrow = true;
+                    break;
+                case Keys.Right:
+                    rightArrow = true;
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Left:
+                    leftArrow = false;
+                    break;
+                case Keys.Right:
+                    rightArrow = false;
+                    break;
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+            }
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed1;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed1;
+            }
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= playerSpeed1;
+            }
+            if (dDown == true && player1.X < this.Height - player1.Height)
+            {
+                player1.X += playerSpeed1;
+            }
+
+            //move player 2 
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= playerSpeed2;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed2;
+
+            }
+            if (leftArrow == true && player2.X > 0)
+            {
+                player2.X -= playerSpeed2;
+            }
+            if (rightArrow == true && player2.X < this.Height - player2.Height)
+            {
+                player2.X += playerSpeed2;
+            }
+
+            // make it so when the players interact with the ball it changes the location and adds a point
+
+            if (player1.IntersectsWith(ball))
+            {
+                ballSpot = ballLoca.Next(1, 11);
+                p1Score.Text = $"{player1Score++}";
+                SoundPlayer bitNoise = new SoundPlayer(Properties.Resources._238283__meroleroman7__8_bit_noise);
+                bitNoise.Play();
+
+
+                // play sound
+
+                // move ball to random spot
+                if (ballSpot == 1)
+                {
+                    ball.X = 180;
+                    ball.Y = 265;
+                }
+                else if (ballSpot == 2)
+                {
+                    ball.X = 129;
+                    ball.Y = 170;
+                }
+                else if (ballSpot == 3)
+                {
+                    ball.X = 92;
+                    ball.Y = 350;
+                }
+                else if (ballSpot == 4)
+                {
+                    ball.X = 35;
+                    ball.Y = 105;
+                }
+                else if (ballSpot == 5)
+                {
+                    ball.X = 159;
+                    ball.Y = 95;
+                }
+                else if (ballSpot == 6)
+                {
+                    ball.X = 295;
+                    ball.Y = 321;
+                }
+                else if (ballSpot == 7)
+                {
+                    ball.X = 299;
+                    ball.Y = 299;
+                }
+                else if (ballSpot == 8)
+                {
+                    ball.X = 21;
+                    ball.Y = 420;
+                }
+                else if (ballSpot == 9)
+                {
+                    ball.X = 12;
+                    ball.Y = 198;
+                }
+                else if (ballSpot == 10)
+                {
+                    ball.X = 10;
+                    ball.Y = 23;
+                }
+            }
+            if (player2.IntersectsWith(ball))
+            {
+                ballSpot = ballLoca.Next(1, 11);
+                p2Score.Text = $"{player2Score++}";
+                SoundPlayer bitNoise = new SoundPlayer(Properties.Resources._238283__meroleroman7__8_bit_noise);
+                bitNoise.Play();
+
+                if (ballSpot == 1)
+                {
+                    ball.X = 180;
+                    ball.Y = 265;
+                }
+                else if (ballSpot == 2)
+                {
+                    ball.X = 129;
+                    ball.Y = 170;
+                }
+                else if (ballSpot == 3)
+                {
+                    ball.X = 92;
+                    ball.Y = 350;
+                }
+                else if (ballSpot == 4)
+                {
+                    ball.X = 35;
+                    ball.Y = 105;
+                }
+                else if (ballSpot == 5)
+                {
+                    ball.X = 159;
+                    ball.Y = 95;
+                }
+                else if (ballSpot == 6)
+                {
+                    ball.X = 295;
+                    ball.Y = 321;
+                }
+                else if (ballSpot == 7)
+                {
+                    ball.X = 299;
+                    ball.Y = 299;
+                }
+                else if (ballSpot == 8)
+                {
+                    ball.X = 21;
+                    ball.Y = 420;
+                }
+                else if (ballSpot == 9)
+                {
+                    ball.X = 12;
+                    ball.Y = 198;
+                }
+                else if (ballSpot == 10)
+                {
+                    ball.X = 10;
+                    ball.Y = 23;
+                }
+
+            }
+
+            if (player1.IntersectsWith(speed))
+            {
+                playerSpeed1++;
+                SoundPlayer bitNoise = new SoundPlayer(Properties.Resources._238283__meroleroman7__8_bit_noise);
+                bitNoise.Play();
+                speedSpot = speedMvmt.Next(1, 11);
+
+
+                if (speedSpot == 1)
+                {
+                    speed.X = 120;
+                    speed.Y = 215;
+                }
+                else if (speedSpot == 2)
+                {
+                    speed.X = 62;
+                    speed.Y = 234;
+                }
+                else if (speedSpot == 3)
+                {
+                    speed.X = 292;
+                    speed.Y = 150;
+                }
+                else if (speedSpot == 4)
+                {
+                    speed.X = 76;
+                    speed.Y = 203;
+                }
+                else if (speedSpot == 5)
+                {
+                    speed.X = 100;
+                    speed.Y = 265;
+                }
+                else if (speedSpot == 6)
+                {
+                    speed.X = 154;
+                    speed.Y = 213;
+                }
+                else if (speedSpot == 7)
+                {
+                    speed.X = 199;
+                    speed.Y = 199;
+                }
+                else if (speedSpot == 8)
+                {
+                    speed.X = 34;
+                    speed.Y = 342;
+                }
+                else if (speedSpot == 9)
+                {
+                    speed.X = 345;
+                    speed.Y = 355;
+                }
+                else if (speedSpot == 10)
+                {
+                    speed.X = 23;
+                    speed.Y = 27;
+                }
+            }
+            if (player2.IntersectsWith(speed))
+            {
+                playerSpeed2++;
+                SoundPlayer bitNoise = new SoundPlayer(Properties.Resources._238283__meroleroman7__8_bit_noise);
+                bitNoise.Play();
+                speedSpot = speedMvmt.Next(1, 11);
+
+                if (speedSpot == 1)
+                {
+                    speed.X = 120;
+                    speed.Y = 215;
+                }
+                else if (speedSpot == 2)
+                {
+                    speed.X = 62;
+                    speed.Y = 234;
+                }
+                else if (speedSpot == 3)
+                {
+                    speed.X = 292;
+                    speed.Y = 150;
+                }
+                else if (speedSpot == 4)
+                {
+                    speed.X = 76;
+                    speed.Y = 203;
+                }
+                else if (speedSpot == 5)
+                {
+                    speed.X = 100;
+                    speed.Y = 265;
+                }
+                else if (speedSpot == 6)
+                {
+                    speed.X = 154;
+                    speed.Y = 213;
+                }
+                else if (speedSpot == 7)
+                {
+                    speed.X = 199;
+                    speed.Y = 199;
+                }
+                else if (speedSpot == 8)
+                {
+                    speed.X = 34;
+                    speed.Y = 342;
+                }
+                else if (speedSpot == 9)
+                {
+                    speed.X = 345;
+                    speed.Y = 355;
+                }
+                else if (speedSpot == 10)
+                {
+                    speed.X = 23;
+                    speed.Y = 27;
+                }
+            }
+
+
+
+            //Check player score and determine the winner
+            if (player1Score == 8)
+            {
+                endLabel.Text = $"Would You Like To Play Again?";
+                noButton.Visible = true;
+                yesButton.Visible = true;
+                SoundPlayer vivtoryNoise = new SoundPlayer(Properties.Resources._462250__silverillusionist__victory_sound_1);
+                vivtoryNoise.Play();
+            }
+            if (player2Score == 8)
+            {
+                endLabel.Text = $"Would You Like To Play Again?";
+                noButton.Visible = true;
+                yesButton.Visible = true;
+                SoundPlayer vivtoryNoise = new SoundPlayer(Properties.Resources._462250__silverillusionist__victory_sound_1);
+                vivtoryNoise.Play();
+            }
+            Refresh();
+
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(SolidBrush, player2);
+            e.Graphics.FillRectangle(whiteBrush, ball);
+            e.Graphics.FillRectangle(YellowBrush, speed);
+
+            // p1Score.Text = $"speed:{speed.X}, {speed.Y}, {speed.Width}, {speed.Height}";
+            //  p2Score.Text = $"p1:{player1.X}, {player1.Y}, {player1.Width}, {player1.Height}";
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+
+}
